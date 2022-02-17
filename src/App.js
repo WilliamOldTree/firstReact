8e5a2a874f108783a4ff43e5479bcf7c82c18df7
WilliamOldTree <dwbarbosa@rd.com.br>
2022-02-17 17:27:04 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import CompPadrao, {Component1, Component2} from './components/Multi'
 import './App.css';
+import Primeiro from './components/Primeiro';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>William Barbosa</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// const App = function(){
+
+//   return <h1>Quero ser Dec</h1>
+// }
+
+// export default App;
+
+
+// export default () => {
+//   return <h1>React exemplo</h1>
+// }
+
+
+// export default () => <h1>Jonas</h1>
+
+// export default function App(){
+//   return(
+//     <Primeiro/>
+//   );
+// }
+
+export default function App(){
+  return(
+    <CompPadrao/>
   );
-}
-
-export default App;
+} 
